01eee95c7f8118fc7b4da20afa936e10a2693d24
jun <jun020216@naver.com>
2022-07-03 21:25:46 +0900

    set jdbc repository

--- src/main/java/jun/junspring/SpringConfig.java
+++ src/main/java/jun/junspring/SpringConfig.java
@@ -1,13 +1,25 @@
 package jun.junspring;
 
+import jun.junspring.repository.JdbcMemberRepository;
 import jun.junspring.repository.MemberRepository;
 import jun.junspring.repository.MemoryMemberRepository;
 import jun.junspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
