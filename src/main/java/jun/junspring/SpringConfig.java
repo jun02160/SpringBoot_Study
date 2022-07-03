6a4311ebcec9b9987b3e5b7a881575f20356c8da
jun <jun20020216@gmail.com>
2022-07-03 13:44:35 +0900

    spring bean(java config)

---
+++ src/main/java/jun/junspring/SpringConfig.java
@@ -0,0 +1,21 @@
+package jun.junspring;
+
+import jun.junspring.repository.MemberRepository;
+import jun.junspring.repository.MemoryMemberRepository;
+import jun.junspring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
